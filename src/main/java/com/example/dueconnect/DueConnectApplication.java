d47d14f1c6d1932d50b914668eac2915f847884f
ccousill <43454787+ccousill@users.noreply.github.com>
2022-10-29 13:27:53 -0400

    Initialization

---
+++ src/main/java/com/example/dueconnect/DueConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.dueconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DueConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DueConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dueconnect/DueConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dueconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DueConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
